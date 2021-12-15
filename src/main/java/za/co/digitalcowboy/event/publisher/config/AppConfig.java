849ded199ee7c052a59e4003433f1c5635708b3a
Luke Lanterme <luke.lanterme@tymedigital.com>
2021-12-15 16:00:21 +0200

    updated package name

--- src/main/java/za/co/digitalcowboy/event/publisher/config/AppConfig.java
+++ src/main/java/za/co/digitalcowboy/event/publisher/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("za.co.couponzilla.rest")
+@ComponentScan("za.co.digitalcowboy.event.publisher")
 public class AppConfig {
 
 

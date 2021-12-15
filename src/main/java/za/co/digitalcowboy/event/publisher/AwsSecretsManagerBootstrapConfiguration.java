78f2172c8d94ab8f8747dffb874f7e24b6c0a119
Luke Lanterme <luke.lanterme@tymedigital.com>
2021-12-15 17:19:43 +0200

    remove secret manager config

--- src/main/java/za/co/digitalcowboy/event/publisher/AwsSecretsManagerBootstrapConfiguration.java
+++ src/main/java/za/co/digitalcowboy/event/publisher/AwsSecretsManagerBootstrapConfiguration.java
@@ -1,21 +1,21 @@
-//package za.co.digitalcowboy.event.publisher;
-//
-//import com.amazonaws.services.secretsmanager.AWSSecretsManager;
-//import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//import org.springframework.cloud.aws.secretsmanager.AwsSecretsManagerProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@EnableConfigurationProperties(AwsSecretsManagerProperties.class)
-//@ConditionalOnClass({ AWSSecretsManager.class })
-//@ConditionalOnProperty(prefix = AwsSecretsManagerProperties.CONFIG_PREFIX, name = "enable", matchIfMissing = true)
-//public class AwsSecretsManagerBootstrapConfiguration {
-//
+package za.co.digitalcowboy.event.publisher;
+
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.aws.secretsmanager.AwsSecretsManagerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(AwsSecretsManagerProperties.class)
+@ConditionalOnClass({ AWSSecretsManager.class })
+@ConditionalOnProperty(prefix = AwsSecretsManagerProperties.CONFIG_PREFIX, name = "enable", matchIfMissing = true)
+public class AwsSecretsManagerBootstrapConfiguration {
+
 //    public static final String REGION ="us-east-2";
 //
 //    @Bean
@@ -23,4 +23,4 @@
 //    AWSSecretsManager smClient() {
 //        return AWSSecretsManagerClientBuilder.standard().withRegion(REGION).build();
 //    }
-//}
+}
